d4bb8fb2356e2c8eefe3771415398ce3b80bb37a
Daniel Passy <danielpassy@hotmail.com>
2023-05-27 19:50:13 -0300

    eslint+prettier

---
+++ front/app/libs/data/http.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+
+const AxiosClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  xsrfHeaderName: 'x-csrftoken',
+  xsrfCookieName: 'csrftoken',
+  withCredentials: true
+})
+
+AxiosClient.defaults.headers.common['Content-Type'] = 'application/json';
+
+export default AxiosClient;
--- front/app/login/page.tsx
+++ front/app/login/page.tsx
@@ -1,55 +1,236 @@
-'use client'
-import { Facebook, Google } from '@mui/icons-material'
-import { Box, Button, Card, CardContent, Divider, Icon, IconButton, TextField, Typography } from '@mui/material'
-import React from 'react'
+'use client';
+import { Facebook, Google, Visibility, VisibilityOff } from '@mui/icons-material';
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Divider,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+  Typography,
+} from '@mui/material';
+import React, { useState } from 'react';
+import api from '@api';
 
+function SelectableTitle({
+  title,
+  LoginSelected,
+  setLoginSelected,
+}: {
+  title: string;
+  LoginSelected: boolean;
+  setLoginSelected: Function;
+}) {
+  return (
+    <Typography
+      sx={{
+        cursor: 'pointer',
+      }}
+      component={'span'}
+      variant="h6"
+      className={LoginSelected ? 'font-light' : 'font-semibold'}
+      onClick={() => setLoginSelected(false)}
+    >
+      {title}
+    </Typography>
+  );
+}
 
 export default function login() {
+  const [LoginSelected, setLoginSelected] = useState(true);
   const centerFlex = {
     display: 'flex',
     justifyContent: 'center',
     flexDirection: 'column',
-  }
+  };
 
   return (
-    <Box sx={{
-      width: { lg: '30vw', sm: '50vw', xs: '90vw' },
-      pt: 3
-    }} >
-      <Card>
-        <CardContent sx={centerFlex}>
-          <Typography variant="h4" component="div">
-            Login
-          </Typography>
-          <Box className='py-2' />
-          <Box className='px-3' sx={centerFlex}>
-            <TextField className='py-2' label="Username" variant="outlined" />
-            <TextField className='py-2' label="Password" variant="outlined" />
-            <Button className='py-2' variant="outlined">
+    <Box
+      sx={{
+        width: {
+          lg: '30vw',
+          sm: '50vw',
+          xs: '90vw',
+        },
+        pt: 3,
+      }}
+    >
+      <Card raised>
+        <CardContent sx={{ ...centerFlex, mx: 3 }}>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-evenly',
+            }}
+          >
+            <Typography
+              sx={{ cursor: 'pointer' }}
+              component={'span'}
+              variant="h6"
+              className={LoginSelected ? 'font-semibold' : 'font-light'}
+              onClick={() => setLoginSelected(true)}
+            >
               Login
-            </Button>
-            <Divider sx={{ my: 3 }} />
-            <Box sx={{ textAlign: 'center' }}>
-              <Typography variant="body2" component="span">
-                Or Login With
-              </Typography>
-            </Box>
-            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-              <Button variant='contained' sx={{ mx: 'auto', backgroundColor: 'blue' }}>
-                <Google sx={{ mr: 1 }} />
-                Google
-              </Button>
-              <Divider sx={{ my: 1.5 }} orientation='vertical' flexItem />
-              <Button color="error" variant='contained' sx={{ mx: 'auto', backgroundColor: 'light-blue' }}>
-                <Facebook sx={{ mr: 1 }} />
-                Facebook
-              </Button>
+            </Typography>
 
-            </Box>
+            <Divider sx={{ mx: 2 }} flexItem orientation="vertical" />
+
+            <SelectableTitle
+              title={'Register'}
+              LoginSelected={LoginSelected}
+              setLoginSelected={setLoginSelected}
+            />
           </Box>
-          {/* <SpaceBar /> */}
+          <Box className="py-2" />
+
+          {LoginSelected ? <LoginForm /> : <RegisterForm />}
+
+          <Divider sx={{ my: 3 }} />
+
+          <Box sx={{ textAlign: 'center', pb: 3 }}>
+            <Typography variant="subtitle2" component="span">
+              Or Login With
+            </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+            <Button
+              color="primary"
+              variant="contained"
+              sx={{ mx: 'auto', backgroundColor: 'blue' }}
+            >
+              <Google sx={{ mr: 1 }} />
+              Google
+            </Button>
+            <Divider sx={{ mx: 2 }} flexItem orientation="vertical" variant="middle" />
+            <Button
+              color="primary"
+              variant="contained"
+              sx={{ mx: 'auto', backgroundColor: 'light-blue' }}
+            >
+              <Facebook sx={{ mr: 1 }} />
+              Facebook
+            </Button>
+          </Box>
         </CardContent>
       </Card>
-    </Box >
-  )
+    </Box>
+  );
+}
+
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const submit = async () => {
+    await api.auth.login(email, password);
+  };
+  return (
+    <>
+      <TextField
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="my-2"
+        label="Email"
+        variant="outlined"
+      />
+
+      {/* Password */}
+      <FormControl className="my-2" variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+        <OutlinedInput
+          type={showPassword ? 'text' : 'password'}
+          label="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton onClick={() => setShowPassword(!showPassword)}>
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+      <Button onClick={submit} className="my-2" variant="outlined">
+        Login
+      </Button>
+    </>
+  );
+}
+
+function RegisterForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const passwordsMatch = password === repeatPassword;
+  const canSubmit = !!(email.length > 5 && password && repeatPassword && passwordsMatch);
+
+  const submit = async () => {
+    await api.auth.register(email, password);
+  };
+
+  return (
+    <>
+      <TextField
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="my-2"
+        label="Email"
+        variant="outlined"
+      />
+
+      {/* Password */}
+      <TextField
+        variant="outlined"
+        type={showPassword ? 'text' : 'password'}
+        label="Password"
+        className="my-2"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton onClick={() => setShowPassword(!showPassword)}>
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+
+      {/* Reapeat Password */}
+      <TextField
+        variant="outlined"
+        error={!passwordsMatch}
+        helperText={passwordsMatch ? null : 'Passwords do not match'}
+        type={showPassword ? 'text' : 'password'}
+        label="Reapeat Password"
+        className="my-2"
+        value={repeatPassword}
+        onChange={(e) => setRepeatPassword(e.target.value)}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton onClick={() => setShowPassword(!showPassword)}>
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Button onClick={submit} disabled={canSubmit} className="my-2" variant="outlined">
+        Register
+      </Button>
+    </>
+  );
 }
