6b1c44fafe17e79b2ec953a363c82f19094cb9ab
Daniel Passy <danielpassy@hotmail.com>
2023-06-19 10:36:12 -0300

    cookie is http only.

--- front/app/libs/data/auth.ts
+++ front/app/libs/data/auth.ts
@@ -2,11 +2,7 @@ import httpClient from '@/app/libs/data/http';
 
 export default {
   login: (email: string, password: string) =>
-    httpClient.post('/auth/login', { email, password }).then((response) => {
-      httpClient.defaults.headers.common[
-        'Authorization'
-      ] = `Bearer ${response.data.token}`;
-    }),
+    httpClient.post('/auth/login', { email, password }),
   register: (email: string, password: string) =>
     httpClient.post('/auth/register', { email, password }),
 };
